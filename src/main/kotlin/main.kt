ff70bd5d2ef3de3178c1b5bf658b3d6b825dede4
daisuke shiraishi <kaiyujin@gmail.com>
2021-06-13 20:29:22 +0900

    add gradle

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,34 @@
+import androidx.compose.desktop.Window
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.IntSize
+import java.awt.image.BufferedImage
+import javax.imageio.ImageIO
+
+fun main() = Window(
+    title = "Search all",
+    size = IntSize(1024, 800),
+    icon = loadImageResource("img/icon.png")
+) {
+    var text by remember { mutableStateOf("Hello, World!") }
+
+    MaterialTheme {
+        Button(onClick = {
+            text = "Hello, Desktop!"
+        }) {
+            Text(text)
+        }
+    }
+}
+
+@Suppress("SameParameterValue")
+private fun loadImageResource(path: String): BufferedImage {
+    val resource = Thread.currentThread().contextClassLoader.getResource(path)
+    requireNotNull(resource) { "Resource $path not found" }
+    return resource.openStream().use(ImageIO::read)
+}
